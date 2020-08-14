06e6674448eeb01651a7c6c530c58f616ed0d4c0
Vaibhavee Shinde <43693189+VaibhaveeM11@users.noreply.github.com>
2020-08-14 23:13:29 +0530

    code  updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,56 @@
 //form validation
- let fname=document.getElementById('fName').value;
- let lname=document.getElementById('lName').value;
+ let fname=document.getElementById('fName');
+ let lname=document.getElementById('lName');
  let classe= document.getElementById('class');
  let yearPass = document.getElementById('yearPass');
- let Percentage = document.getElementById('Percentage');
+ let Percentage = document.getElementById('Percentage').value;
  let submit = document.getElementById('btn-submit');
- const maxNameLength =20;
- const Numformat= '^[0-9]*$';
-  FormCheck=()=>
+
+ //to show erros msgs
+ let statusBox =document.getElementById('statusBox');
+  FormCheck=(e)=>
   {
     let showStatus=[];
-    // To check all fields are not empty
-
-      //1.Name (accepts only alphabets a-zA-Z, max character size:20)
-    if(fname=='' && lname=='' && classe=='' && yearPass=='' && Percentage=='')
-    {
-           showStatus.push('fill all field' );  
-    }
-     if(fname.maxlength==20 && lname.maxlength==20)
-    {
+     //Name (accepts only alphabets a-zA-Z, max character size:20)
+      if(fname.length<=20 ||fname.value==='')
+     {
+       // alert('Invalid Last Name');       
+         showStatus.push('Enter Name upto Max 20 leg');    
+     }
+     // Last Name (accepts only alphabets a-zA-Z, max character size:20)
+     if(lname.lenghth<=20 ||lname.value==='')
+     {
+        // alert('Invalid Last Name');
         showStatus.push('Enter Name upto Max 20 leg');    
+     }
+     // Class (accepts both numeric and string values, eg. 5A)
 
-    }
-   if( classe=='')
+//    if( classe=='')
+//     {
+
+//     }
+//  Year of passing (accepts only number, any year after 2017 should be invalid)
+
+// if( !Number(yearPass))
+// {
+//     alert('invalid  year');
+// }
+
+
+// Percentage of marks (accepts only number)
+     if( !Number(Percentage) || Percentage=='')
     {
-
+        //alert('invalid Percentage');
+        showStatus.push('invalid Percentage');
     }
-     if( yearPass=='')
-    {
 
-    }
-     if( Number(Percentage)==true)
-    {
+    if(showStatus.length>0)
+    {         e.preventDefault();
 
+        statusBox.innerText=showStatus.join(',');
     }
-    // else
-    // {
-    //  console.log('done');
-    // }
   }
 
   submit.addEventListener('click',FormCheck)
-  console.log('insidemform');
 
 
-// 2. Last Name (accepts only alphabets a-zA-Z, max character size:20)
-// 3. Class (accepts both numeric and string values, eg. 5A)
-// 4. Year of passing (accepts only number, any year after 2017 should be invalid)
-// 5. Percentage of marks (accepts only number)

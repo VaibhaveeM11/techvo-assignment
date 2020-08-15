8b7424595d9d090da5f4154d68834c60439ac6a9
Vaibhavee Shinde <43693189+VaibhaveeM11@users.noreply.github.com>
2020-08-15 15:48:08 +0530

    code updated

--- studentScore.js
+++ studentScore.js
@@ -3,7 +3,6 @@
 //Maximum marks in each subject is 50.
 //Minimum marks required to pass any subject is 20.
 //Total marks is the sum of marks received in all subjects.
-
  fetch('./stud.json', 
  {
     headers : { 
@@ -14,39 +13,51 @@
   })
 .then((res)=>res.json())
 .then((data)=>{
-    if(data.length>0)
+   
+  if(data.length>0)
     {
       let output='';
       data.forEach( i => {
-         //To calculate total  Score
+        //to sort name 
+        let name=i.name
+  //To calculate total  Score
               let Maths=i.marks["Maths"];
               let English=i.marks["English"];
               let Science=i.marks["Science"];
+
            let total=Maths+English+Science;
-           //To show status total  Score
-           let status='none'
         
-          // if(i.total>)
-          // {
-            
-          // }
-        // let name=i.name.sort(function(a,b){
-        //      if(a.name>b.name)
-        //      {
-        //        return b.name
-        //      }
-        //      else
-        //      {
-        //       return a.name
-        //      }
-        //})
-        output+=`<tr>
-                <td id='studName'>${i.name.sort(function(a,b){}</td>
+  //To show status total  Score
+           let status='';
+           //for topper
+           //let  top = total+1>total?total+1:total;
+        if(Maths>=20 && English>=20 && Science>=20)
+        {
+           status='pass';
+        }
+          
+         else if(top==150 )
+         {
+             status="topper";
+          }
+        else
+        {
+          status = "fail";
+        }
+    
+          // .sort(function(a,b){
+          //   if(a.name>b.name) return b.name
+              //  else return a.name}           
+ output+= `<tr>
+                <td id='studName'>${name}</td>
                 <td>${i.rollNumber}</td>
                 <td>${total}</td>
-                <td>${status}</td>
-                </tr>`;
+                <td>
+                   ${status}
+                </td>
+         </tr>`;
       });
-      document.getElementById('tablebody').innerHTML=output;
+
+   document.getElementById('tablebody').innerHTML=output;
     }
  });
